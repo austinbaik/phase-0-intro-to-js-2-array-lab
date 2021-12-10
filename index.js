4813136d47be25a978fa87cd4dda2fb9ebc2d3d3
Austin Baik <baik.austin@gmail.com>
2021-12-09 18:02:15 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const CopyCats = [...cats, name];
+    return CopyCats;
+}
+
+function prependCat(name){
+    const CopyCats = [name, ...cats]
+    return CopyCats;
+}
+
+function removeLastCat(){
+    const CopyCats = cats.slice(0, -1);
+    return CopyCats;
+}
+
+function removeFirstCat(){
+    const CopyCats = cats.slice(1);
+    return CopyCats;
+}
